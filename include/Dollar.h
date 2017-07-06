f5f29dff36ecf2dfca5c16bc3cd041cc854549a5
FSGuizi <fsguizi@hotmail.com>
2017-07-06 11:45:52 -0300

    Corrected indentention

--- include/Dollar.h
+++ include/Dollar.h
@@ -4,7 +4,7 @@
                                                                                                               
 class Dollar                                                                                                  
 {
-	public:                                                                                                          
+public:                                                                                                          
 Dollar();                                                                                                 
 ~Dollar();                                                                                           
 };                                                                                                            
